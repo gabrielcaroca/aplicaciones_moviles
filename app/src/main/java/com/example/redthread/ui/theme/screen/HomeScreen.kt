4e70c3710def305435fa03e16340f4fa00f6434e
Felipe Flores <felipeandresfloresmoya@gmail.com>
2025-09-25 10:41:49 -0300

    fix

--- app/src/main/java/com/example/redthread/ui/theme/screen/HomeScreen.kt
+++ app/src/main/java/com/example/redthread/ui/theme/screen/HomeScreen.kt
@@ -1,10 +1,10 @@
 package com.example.redthread.ui.theme.screen
 
-import android.widget.Button
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,25 +12,24 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Button
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(){
-    Scaffold (
-        topBar = {
-            TopAppBar(title = { Text(text = "RedThread")})
-        }
+fun HomeScreen(onButtonClick: () -> Unit = {}) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("RedThread") }) }
     ) { innerPadding ->
-        Column (
+        Column(
             modifier = Modifier
-                .padding()
                 .fillMaxSize()
+                .padding(innerPadding)
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(23.dp)
         ) {
             Text(text = "¡Bienvenido!")
-            Button(onClick = { })
+            Button(onClick = onButtonClick) {
+                Text("Entrar")
+            }
         }
     }
 }
